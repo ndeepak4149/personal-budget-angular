3367799e133d18e2f0975e2b7118b8b507e3efe8
unknown <you@example.com>
2024-02-03 22:57:34 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -2,33 +2,31 @@
 
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
-
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
-
+app.use(cors());
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    // Read the content of the JSON file
+    fs.readFile('exercise01.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+
+        try {
+            const budgetData = JSON.parse(data);
+            res.json(budgetData);
+        } catch (parseError) {
+            console.error(parseError);
+            res.status(500).json({ error: 'Error parsing JSON data' });
+        }
+    });
 });
 
-app .listen(port, () => {
+app.listen(port, () => {
     console.log(`API served at http://localhost:${port}`);
 });
