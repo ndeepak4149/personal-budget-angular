c6b43eb6647a1d270b1eec5ab3dfa0bec1ee9efa
unknown <you@example.com>
2024-02-11 20:44:15 -0500

    added angular

---
+++ personal-budget/src/app/article/article.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'pb-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss']
+})
+export class ArticleComponent {
+
+
+
+}
